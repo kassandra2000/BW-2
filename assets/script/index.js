7e89cba892f15ed4c2cd77749a507fde3436a450
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-04-19 12:04:26 +0200

    index-reponsive

--- assets/script/index.js
+++ assets/script/index.js
@@ -103,7 +103,7 @@ const album = (url) => {
       }
       if (playlistCounter < 6) {
         const div = document.createElement("div");
-        div.classList.add("ps-1", "col-4");
+        div.classList.add("ps-1","col-12", "col-sm-6","col-md-4");
 
         div.innerHTML = `
         <div onclick="locationId(${data.id})" class="row flex col-bg g-1 img-container pointer">
