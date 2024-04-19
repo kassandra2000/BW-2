2e3553e2a30620dfd756650e9ce9b40bd203b017
Giovanni Ghirardelli <gianni_96@hotmail.it>
2024-04-19 09:11:08 +0200

    add error correction

--- assets/script/general.js
+++ assets/script/general.js
@@ -1,17 +1,20 @@
-const URL3 = "https://deezerdevs-deezer.p.rapidapi.com/track/2465337715";
+const URL3 = "https://deezerdevs-deezer.p.rapidapi.com/track/";
 const audio = document.getElementById("audio");
 const titleAdd = document.getElementById("title");
 const artistAdd = document.querySelector(".artist");
 const previewAdd = document.getElementById("preview");
-const playBtn = document.querySelector(".bi-play-circle-fill");
+const playBtn = document.getElementById("play");
 const img = document.getElementById("img");
+let audio1 = undefined;
 let i = 3.33;
 let time = 0;
+let interval = 0;
 const bar = document.querySelector(".progress");
 console.log("");
 
-const mediaPlayer = () => {
-  fetch(URL3, {
+const mediaPlayer = (id) => {
+  const URLid = URL3 + id;
+  fetch(URLid, {
     headers: {
       "X-RapidAPI-Key": "ee479080e1msh75d5fd4295dec8ap18b484jsnc600f214b6d8",
       "X-RapidAPI-Host": "deezerdevs-deezer.p.rapidapi.com",
@@ -27,6 +30,13 @@ const mediaPlayer = () => {
     })
     .then((elem) => {
       console.log(elem);
+      if (audio1 !== undefined) {
+        if (!audio1.paused) {
+          audio1.pause();
+
+          interval = 0;
+        }
+      }
 
       const cover = elem.album.cover;
       const title = elem.title;
@@ -35,10 +45,10 @@ const mediaPlayer = () => {
       titleAdd.innerHTML = `${title}`;
       artistAdd.innerHTML = `${artist}`;
       img.src = `${cover}`;
-      const audio = new Audio(preview);
-      let interval = 0;
+      audio1 = new Audio(preview);
+
       playBtn.addEventListener("click", () => {
-        if (interval === 0 || audio.paused) {
+        if (interval === 0 || audio1.paused) {
           interval = setInterval(() => {
             if (time < 99) {
               time += i;
@@ -50,10 +60,10 @@ const mediaPlayer = () => {
           }, 1000);
         }
 
-        if (audio.paused) {
-          audio.play();
+        if (audio1.paused) {
+          audio1.play();
         } else {
-          audio.pause();
+          audio1.pause();
           clearInterval(interval);
           console.log(interval);
           time += 3.33;
@@ -61,14 +71,17 @@ const mediaPlayer = () => {
 
         const volumeSlider = document.getElementById("volume-slider");
         volumeSlider.addEventListener("input", () => {
-          console.log(audio.volume);
-          audio.volume = volumeSlider.value;
+          console.log(audio1.volume);
+          audio1.volume = volumeSlider.value;
 
-          console.log(audio.volume);
-          console.dir(audio);
+          console.log(audio1.volume);
+          console.dir(audio1);
         });
       });
 
       console.log(titleAdd);
+    })
+    .catch((err) => {
+      console.log(err);
     });
 };
